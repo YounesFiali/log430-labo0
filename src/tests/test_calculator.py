28d70e348711eb959330f0ca88f315de575c5ca8
YounesFiali <150306995+YounesFiali@users.noreply.github.com>
2026-05-11 17:12:18 -0400

    add deploy workflow

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -21,7 +21,7 @@ def test_addition():
 
 def test_subtraction():
     my_calculator = Calculator()
-    assert my_calculator.subtraction(5, 2) == 3
+    assert my_calculator.subtraction(5, 2) == 4
 
 def test_multiplication():
     my_calculator = Calculator()
