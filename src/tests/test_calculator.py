6619a0e0420da4c90dd8250175df9eb58839ea65
YounesFiali <150306995+YounesFiali@users.noreply.github.com>
2026-05-11 14:31:00 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -11,4 +11,27 @@ def test_app():
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
-# TODO: ajoutez les tests
+def test_init():
+    my_calculator = Calculator()
+    assert my_calculator is not None
+
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 2) == 3
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(4, 3) == 12
+
+def test_division():
+    my_calculator = Calculator()
+    assert my_calculator.division(10, 2) == 5
+
+def test_division_by_zero():
+    my_calculator = Calculator()
+    assert my_calculator.division(10, 0) == "Erreur : division par zéro"
+    assert my_calculator.last_result == "Error"
